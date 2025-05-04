44421cfe2fb4326ae037580f46a78c39c7360b2c
lbchs <534365726@qq.com>
2025-05-04 08:46:48 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.setruth.yangdialog"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
